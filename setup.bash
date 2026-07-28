d1becf8617013333f7a3c1d91ae03c8b4063e6cc
Jonathan Holley <holleyjonathan5@gmail.com>
2026-07-28 02:13:03 +0000

    setup check for os types

--- setup.bash
+++ setup.bash
@@ -3,7 +3,25 @@
 echo "Welcome to Light setup!"
 
 if [[ ! command -v g++ || ! command -v gcc ]]; then
-  echo "[Setup] gcc isn't installed, cannot keep going forward"
+  echo "[Setup] gcc isn't installed, cannot keep going forward until you install gcc..."
+  echo "---Ways to install GCC---"
+  echo "MSYS2 Windows: pacman -S gcc"
+  echo "Arch: sudo pacman -S gcc"
+  echo
+  exit 1
 fi
 
-if 
+echo "---[WELCOME TO LIGHT SETUP]---"
+wait 1
+
+echo "[0/1] Checking operating system..."
+case "$OSTYPE" in
+	linux*) OPSYS="linux" ;;
+	darwin*) OPSYS="macos" ;;
+	msys*) OPSYS="windows" ;;
+	cygwin*) OPSYS="windows" ;;
+	*) OPSYS="UNKNOWN" && echo "Cannot find what specifically $OSTYPE is..." && exit 1 ;;
+esac
+
+echo "[1/1] $OPSYS FOUND"
+
