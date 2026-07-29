fe081971e49a3b6f7feb553a49f731295faf2f8f
Jonathan Holley <holleyjonathan5@gmail.com>
2026-07-29 00:29:07 +0000

    s

--- do.sh
+++ do.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "git commit [a]"
+echo "git commit [a] go to repo [s]"
 echo
 read -n 1 -p "command: " userinput
 echo
 
 if [[ $userinput == "a" ]]; then
 	read -p "Message: " gitmsg
-	git add . && git commit -m gitmsg
+	git add . && git commit -m $gitmsg
 	git push origin main
+elif [[ $userinput == "s" ]]; then
+	chromium github.com/midlevelgamedev/light &> /dev/null &
 fi
